eecefa1505e6145acf6ab1df6994e9a91c45b7e8
Quanuanc <quanuanc@outlook.com>
2023-04-18 22:39:03 +0800

    Compile source string to class file

--- src/test/java/cheng/demo/CompilerDemo.java
+++ src/test/java/cheng/demo/CompilerDemo.java
@@ -1,32 +1,55 @@
 package cheng.demo;
 
 import javax.tools.*;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CompilerDemo {
     private static final String source = """
-                public class HelloWorld{
+                public class HelloWorld2 {
                     public static void main(String[] args){
                         System.out.println("Hello world!");
+                        System.out.println("Hello world2!");
                     }
                 }
             """;
+
+    private static JavaFileObject tempJavaFileObject;
+    private static final Pattern CLASS_PATTERN = Pattern.compile("(?:public\\s+)?class\\s+(\\w+)");
 
     public static void main(String[] args) {
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<>();
         JavaFileManager javaFileManager = new TmpJavaFileManager(compiler.getStandardFileManager(collector, null, null));
 
-        String className = "HelloWorld";
+        String className = null;
+        Matcher matcher = CLASS_PATTERN.matcher(source);
+        if (matcher.find()) {
+            className = matcher.group(1);
+        }
+
 
         JavaFileObject sourceJavaFileObject = new TmpJavaFileObject(className, source);
         Boolean result = compiler.getTask(null, javaFileManager, collector, null, null, List.of(sourceJavaFileObject)).call();
         if (!result) {
             List<Diagnostic<? extends JavaFileObject>> diagnostics = collector.getDiagnostics();
             diagnostics.forEach(System.out::println);
+        }
+
+        if (tempJavaFileObject != null) {
+            TmpJavaFileObject javaFileObject = (TmpJavaFileObject) tempJavaFileObject;
+            byte[] classBytes = javaFileObject.getClassBytes();
+            File file = new File(String.format("target/classes/%s.class", className));
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(classBytes, 0, classBytes.length);
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -38,16 +61,19 @@ public class CompilerDemo {
 
         @Override
         public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) {
-            return new TmpJavaFileObject(className, source);
+            tempJavaFileObject = new TmpJavaFileObject(className, source);
+            return tempJavaFileObject;
         }
     }
 
     static class TmpJavaFileObject extends SimpleJavaFileObject {
         private final String source;
+        private final ByteArrayOutputStream outputStream;
 
         public TmpJavaFileObject(String name, String source) {
-            super(URI.create("String:///" + name + Kind.SOURCE.extension), Kind.SOURCE);
+            super(URI.create("prefixDemo:/" + name + Kind.SOURCE.extension), Kind.SOURCE);
             this.source = source;
+            outputStream = new ByteArrayOutputStream();
         }
 
         @Override
@@ -59,7 +85,11 @@ public class CompilerDemo {
 
         @Override
         public OutputStream openOutputStream() {
-            return new ByteArrayOutputStream();
+            return outputStream;
+        }
+
+        public byte[] getClassBytes() {
+            return outputStream.toByteArray();
         }
     }
 }
